36368e0393732e7749d07589ee1769c77c02514d
Ashutosh Jha <ashutosh.jha@nagarro.com>
2019-08-23 16:26:38 +0530

    static site

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// set path for static assets
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/health', (req, res) => res.status(200).send('Good!'))
 
